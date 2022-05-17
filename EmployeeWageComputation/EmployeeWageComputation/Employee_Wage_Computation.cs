507b5ed519e64297a5af480a1c5f9eb310a09207
chinki kumari singh <singhchinkikumari@gmail.com>
2022-05-17 22:37:40 +0530

    [chinki] Calculate wages till condition

--- EmployeeWageComputation/EmployeeWageComputation/Employee_Wage_Computation.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Employee_Wage_Computation.cs
@@ -12,12 +12,14 @@ namespace EmployeeWageComputation
         public const int IS_PART_TIME = 1;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HOURS_IN_MONTH = 100;
         static void Main(string[] args)
         {
-            int empHrs = 0; 
-            int empWage = 0;
-            int totalEmpWages=0;
-            for (int i = 0; i < NUM_OF_WORKING_DAYS; i++)
+            int empHrs = 0;
+            int totalEmpWages;
+            int totalWorkingDays =0;
+            int totalEmployeeHour = 0;
+            while (empHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS)
             {
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
@@ -33,10 +35,12 @@ namespace EmployeeWageComputation
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWages = totalEmpWages + empWage;
-                Console.WriteLine("Employee wages is=" + empWage);
+
+                totalEmployeeHour += empHrs;
+                Console.WriteLine("Day=" + totalWorkingDays + " Emplyee hours=" + empHrs);
+                totalWorkingDays++;
             }
+            totalEmpWages = totalEmployeeHour * EMP_RATE_PER_HOUR;
             Console.WriteLine("total employee wage is=" + totalEmpWages);
         }
     }
