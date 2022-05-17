7ab609f8f2ce4953ab14e7f9761134e97d58228e
chinki kumari singh <singhchinkikumari@gmail.com>
2022-05-17 19:55:45 +0530

    [chinki] Check Whether Employee is Absent or Present

---
+++ EmployeeWageComputation/EmployeeWageComputation/Employee_Wage_Computation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class Employee_Wage_Computation
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
